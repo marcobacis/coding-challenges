bc61b6fdf57c1c44f4ca1400137780adc549bbfa
Marco Bacis <circu.dev@gmail.com>
2023-08-12 13:57:33 +0200

    refactor with parser, add read array

--- rjson/src/lib.rs
+++ rjson/src/lib.rs
@@ -35,191 +35,259 @@ pub enum JsonError {
     /// A number that is invalid in json, but is a perfectly fine
     /// floating-point number.
     InvalidNumber,
+    UnterminatedArray,
 }
 
 impl JsonObject {
     pub fn read(s: &str) -> Result<Self, JsonError> {
-        Self::partial_read(&s.chars().collect::<Vec<_>>(), 0).map(|(x, _)| x)
+        let mut parser = Parser::new(s);
+        parser.partial_read()
+    }
+}
+
+struct Parser {
+    buf: Vec<char>,
+    index: usize,
+}
+
+impl Parser {
+    fn new(str: &str) -> Self {
+        Parser {
+            buf: str.chars().collect(),
+            index: 0,
+        }
+    }
+
+    pub fn skip_whitespace(&mut self) {
+        while self.buf.len() > self.index && self.buf[self.index].is_whitespace() {
+            self.index += 1;
+        }
+    }
+
+    fn end_reached(&self) -> bool {
+        self.index >= self.buf.len()
+    }
+
+    fn peek(&self) -> Option<char> {
+        if self.end_reached() {
+            return None;
+        }
+        Some(self.buf[self.index])
+    }
+
+    fn next(&mut self) -> Option<char> {
+        if self.end_reached() {
+            return None;
+        }
+        self.index += 1;
+        while let Some(c) = self.peek() {
+            if !c.is_whitespace() {
+                return Some(c);
+            }
+            self.index += 1;
+        }
+        None
+    }
+
+    fn next_no_skip(&mut self) -> Option<char> {
+        if self.end_reached() {
+            return None;
+        }
+        self.index += 1;
+        self.peek()
     }
 
     /// Reads a json object partially, given its string representation and an index from
     /// where to start reading
-    fn partial_read(s: &[char], index: usize) -> Result<(Self, usize), JsonError> {
-        let mut newindex = index;
-        newindex = Self::skip_whitespace(s, newindex);
-
-        if newindex >= s.len() {
+    fn partial_read(&mut self) -> Result<JsonObject, JsonError> {
+        self.skip_whitespace();
+        if let Some(c) = self.peek() {
+            match c {
+                '[' => self.partial_read_array(),
+                '0'..='9' | '+' | '-' => self.partial_read_number(),
+                '"' => self.partial_read_string(),
+                'f' => self.partial_read_false(),
+                't' => self.partial_read_true(),
+                'n' => self.partial_read_null(),
+                _ => Err(JsonError::InvalidChar(self.buf[self.index], self.index)),
+            }
+        } else {
             return Err(JsonError::Empty);
         }
-
-        match s[newindex] {
-            '0'..='9' | '+' | '-' => Self::partial_read_number(s, newindex),
-            '"' => Self::partial_read_string(s, newindex),
-            'f' => Self::partial_read_false(s, newindex),
-            't' => Self::partial_read_true(s, newindex),
-            'n' => Self::partial_read_null(s, newindex),
-            _ => Err(JsonError::InvalidChar(s[newindex], newindex)),
-        }
-    }
-
-    fn skip_whitespace(s: &[char], index: usize) -> usize {
-        let mut newindex = index;
-        while s.len() > newindex && s[newindex].is_whitespace() {
-            newindex += 1;
-        }
-        newindex
     }
 
     // Reads a jsoon number object from a given index
-    fn partial_read_number(s: &[char], index: usize) -> Result<(Self, usize), JsonError> {
-        let startindex = index;
-        let mut newindex = index;
-        newindex = Self::skip_whitespace(s, newindex);
+    fn partial_read_number(&mut self) -> Result<JsonObject, JsonError> {
+        let mut token = String::new();
+
+        self.skip_whitespace();
 
         // Isolate string containing number
         // Read sign
-        if s.len() > newindex && (s[newindex] == '+' || s[newindex] == '-') {
-            newindex += 1;
+        if let Some(c @ ('+' | '-')) = self.peek() {
+            token.push(c);
+            self.next_no_skip();
         }
 
         // Read integer
-        while s.len() > newindex && s[newindex].is_digit(10) {
-            newindex += 1;
+        while let Some(c @ '0'..='9') = self.peek() {
+            token.push(c);
+            self.next_no_skip();
         }
 
         // Read fraction
-        if s.len() > newindex && s[newindex] == '.' {
-            newindex += 1;
-            while s.len() > newindex && s[newindex].is_digit(10) {
-                newindex += 1;
+        if let Some(c @ '.') = self.peek() {
+            token.push(c);
+            while let Some(f @ '0'..='9') = self.next_no_skip() {
+                token.push(f);
             }
         }
 
         // Read exponent
-        if s.len() > newindex && s[newindex].to_ascii_lowercase() == 'e' {
-            newindex += 1;
-            while s.len() > newindex && s[newindex].is_digit(10) {
-                newindex += 1;
+        if let Some(c @ ('e' | 'E')) = self.peek() {
+            token.push(c);
+            while let Some(f @ '0'..='9') = self.next_no_skip() {
+                token.push(f);
             }
         }
-        let token = &s[startindex..newindex].iter().collect::<String>();
 
         let regex = Regex::new(r"[+-]?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?$").unwrap();
-        if !regex.is_match(token) {
+        if !regex.is_match(&token.as_str()) {
             return Err(JsonError::InvalidNumber);
         }
 
-        let num = f64::from_str(token);
+        let num = f64::from_str(&token);
         match num {
-            Ok(num) => Ok((Self::Number(num), newindex)),
+            Ok(num) => Ok(JsonObject::Number(num)),
             Err(_) => Err(JsonError::InvalidNumber),
         }
     }
 
     fn partial_read_given_string(
-        s: &[char],
-        index: usize,
+        &mut self,
         goal: &str,
-        value: Self,
-    ) -> Result<(Self, usize), JsonError> {
-        if s.len() < index + goal.len() {
+        value: JsonObject,
+    ) -> Result<JsonObject, JsonError> {
+        if self.buf.len() < self.index + goal.len() {
             return Err(JsonError::Empty); // Might have another name for the error
         }
 
-        let mut newindex = index;
-        let word = goal.chars().collect::<Vec<_>>();
-        let mut i = 0;
-        while s.len() > newindex && i < word.len() && s[newindex] == word[i] {
-            newindex += 1;
-            i += 1;
+        for g in goal.chars() {
+            if let Some(c) = self.peek() {
+                if c != g {
+                    return Err(JsonError::InvalidChar(c, self.index));
+                }
+                self.next_no_skip();
+            }
         }
-
-        if i == word.len() {
-            Ok((value, newindex))
-        } else {
-            Err(JsonError::InvalidChar(s[newindex], newindex))
-        }
+        Ok(value)
     }
 
-    fn partial_read_false(s: &[char], index: usize) -> Result<(Self, usize), JsonError> {
-        Self::partial_read_given_string(s, index, "false", Self::Bool(false))
+    fn partial_read_false(&mut self) -> Result<JsonObject, JsonError> {
+        self.partial_read_given_string("false", JsonObject::Bool(false))
     }
 
-    fn partial_read_true(s: &[char], index: usize) -> Result<(Self, usize), JsonError> {
-        Self::partial_read_given_string(s, index, "true", Self::Bool(true))
+    fn partial_read_true(&mut self) -> Result<JsonObject, JsonError> {
+        self.partial_read_given_string("true", JsonObject::Bool(true))
     }
 
-    fn partial_read_null(s: &[char], index: usize) -> Result<(Self, usize), JsonError> {
-        Self::partial_read_given_string(s, index, "null", Self::Null)
+    fn partial_read_null(&mut self) -> Result<JsonObject, JsonError> {
+        self.partial_read_given_string("null", JsonObject::Null)
     }
 
-    fn partial_read_string(s: &[char], index: usize) -> Result<(Self, usize), JsonError> {
-        let mut newindex = index;
-        newindex = Self::skip_whitespace(s, newindex);
-        newindex += 1;
-        let startindex = newindex;
-
-        let mut str = String::new();
+    fn partial_read_string(&mut self) -> Result<JsonObject, JsonError> {
+        self.skip_whitespace();
         let mut utf16: Vec<u16> = Vec::new();
 
-        while s.len() > newindex && s[newindex] != '"' {
-            if s[newindex] == '\\' {
-                newindex += 1;
-                if s.len() <= newindex {
-                    return Err(JsonError::EndedOnEscape);
-                }
-
-                // Manage escape character
-                let mut utf16char = [0u16; 2];
-                match s[newindex] {
-                    '\\' => utf16.extend_from_slice('\\'.encode_utf16(&mut utf16char)),
-                    '/' => utf16.extend_from_slice('/'.encode_utf16(&mut utf16char)),
-                    '"' => utf16.extend_from_slice('"'.encode_utf16(&mut utf16char)),
-                    'b' => utf16.extend_from_slice('\u{0008}'.encode_utf16(&mut utf16char)),
-                    'f' => utf16.extend_from_slice('\u{000c}'.encode_utf16(&mut utf16char)),
-                    'n' => utf16.extend_from_slice('\n'.encode_utf16(&mut utf16char)),
-                    'r' => utf16.extend_from_slice('\r'.encode_utf16(&mut utf16char)),
-                    't' => utf16.extend_from_slice('\t'.encode_utf16(&mut utf16char)),
-                    // Manage unicode coodes \uXXXX
-                    'u' => {
-                        let mut u = 0u16;
-                        for _ in 0..4 {
-                            newindex += 1;
-                            if s.len() <= newindex {
-                                return Err(JsonError::InvalidCodepoint);
-                            }
-                            if let Some(h) = s[newindex].to_digit(16) {
-                                u = u * 0x10 + h as u16;
-                            } else {
-                                return Err(JsonError::InvalidCodepoint);
-                            }
-                        }
-                        utf16.push(u);
+        while let Some(c) = self.next_no_skip() {
+            match c {
+                '\\' => {
+                    if self.end_reached() {
+                        return Err(JsonError::EndedOnEscape);
                     }
-                    invalid_char => return Err(JsonError::InvalidChar(invalid_char, newindex)),
-                };
-            } else {
-                let mut buf = [0u16; 2];
-                utf16.extend_from_slice(s[newindex].encode_utf16(&mut buf));
+
+                    // Manage escape character
+                    let mut utf16char = [0u16; 2];
+                    match self.next_no_skip() {
+                        Some('\\') => utf16.extend_from_slice('\\'.encode_utf16(&mut utf16char)),
+                        Some('/') => utf16.extend_from_slice('/'.encode_utf16(&mut utf16char)),
+                        Some('"') => utf16.extend_from_slice('"'.encode_utf16(&mut utf16char)),
+                        Some('b') => {
+                            utf16.extend_from_slice('\u{0008}'.encode_utf16(&mut utf16char))
+                        }
+                        Some('f') => {
+                            utf16.extend_from_slice('\u{000c}'.encode_utf16(&mut utf16char))
+                        }
+                        Some('n') => utf16.extend_from_slice('\n'.encode_utf16(&mut utf16char)),
+                        Some('r') => utf16.extend_from_slice('\r'.encode_utf16(&mut utf16char)),
+                        Some('t') => utf16.extend_from_slice('\t'.encode_utf16(&mut utf16char)),
+                        //Manage unicode coodes \uXXXX
+                        Some('u') => {
+                            let mut u = 0u16;
+                            for _ in 0..4 {
+                                match self.next_no_skip() {
+                                    Some(c) => {
+                                        if let Some(h) = c.to_digit(16) {
+                                            u = u * 0x10 + h as u16;
+                                        } else {
+                                            return Err(JsonError::InvalidCodepoint);
+                                        }
+                                    }
+                                    None => return Err(JsonError::InvalidCodepoint),
+                                }
+                            }
+                            utf16.push(u);
+                        }
+                        Some(c) => return Err(JsonError::InvalidChar(c, self.index)),
+                        None => return Err(JsonError::Empty),
+                    };
+                }
+                '"' => {
+                    self.next_no_skip();
+                    return Ok(JsonObject::JsonString(String::from_utf16(&utf16).unwrap()));
+                }
+                _ => {
+                    let mut buf = [0u16; 2];
+                    utf16.extend_from_slice(c.encode_utf16(&mut buf));
+                }
             }
-            newindex += 1;
-        }
-        if s[newindex] != '"' {
-            return Err(JsonError::UnterminatedString);
         }
 
-        let endindex = newindex;
-        Ok((
-            Self::JsonString(String::from_utf16(&utf16).unwrap()),
-            newindex + 1,
-        ))
+        println!("Shouldn't go there");
+
+        Err(JsonError::UnterminatedString)
+    }
+
+    fn partial_read_array(&mut self) -> Result<JsonObject, JsonError> {
+        let mut elements: Vec<JsonObject> = Vec::new();
+        self.next_no_skip();
+        self.skip_whitespace();
+        loop {
+            self.skip_whitespace();
+            match self.peek() {
+                Some(',') => {
+                    if let Some(c @ (',' | ']')) = self.next() {
+                        return Err(JsonError::InvalidChar(c, self.index));
+                    }
+                }
+                Some(']') => return Ok(JsonObject::Array(elements)),
+                Some(c) => {
+                    let elem = self.partial_read();
+                    match elem {
+                        Ok(e) => elements.push(e),
+                        Err(err) => return Err(err),
+                    }
+                }
+                None => return Err(JsonError::UnterminatedArray),
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::JsonError;
-    use crate::JsonObject::{self, Bool, JsonString, Null, Number};
+    use crate::JsonObject::{self, Array, Bool, JsonString, Null, Number};
 
     #[test]
     fn test_empty() {
@@ -284,5 +352,26 @@ mod tests {
                 JsonString(output.to_string())
             );
         }
+    }
+
+    #[test]
+    fn test_read_array() {
+        assert_eq!(JsonObject::read("[]").unwrap(), Array(Vec::new()));
+        assert_eq!(
+            JsonObject::read("[1,2]").unwrap(),
+            Array(vec![Number(1.0), Number(2.0)])
+        );
+        assert_eq!(
+            JsonObject::read("[3,]"),
+            Err(JsonError::InvalidChar(']', 3))
+        );
+        assert_eq!(
+            JsonObject::read("[3, , 3.2]"),
+            Err(JsonError::InvalidChar(',', 4))
+        );
+        assert_eq!(
+            JsonObject::read("[\"ciao\", 5.423]").unwrap(),
+            Array(vec![JsonString("ciao".to_string()), Number(5.423)])
+        );
     }
 }
